4d59c5e4d1ecaf5e6f7e8d313488324ef7ab2344
j4ik0-dev <salazzarjefferson@gmail.com>
2026-01-19 16:50:38 -0600

    modificacion de rutas al index

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,33 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import HomePage from '../views/HomePage.vue';
+import LoginPage from '../views/LoginPage.vue';
+import RegisterPage from '../views/RegisterPage.vue';
+import AuthService from '@/services/AuthService';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: LoginPage
+  },
+  {
+    path: '/register',
+    component: RegisterPage
   },
   {
     path: '/home',
-    name: 'Home',
-    component: HomePage
+    component: HomePage,
+    beforeEnter: (to, from, next) => {
+      if (AuthService.isLoggedIn()) {
+        next();
+      } else {
+        next('/login');
+      }
+    }
   }
 ]
 
